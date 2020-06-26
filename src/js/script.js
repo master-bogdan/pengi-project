a4813856049c481a955ff46cb27354fc98f9a0f7
masterbogdan <maiskiychavk@gmail.com>
2020-06-26 22:56:16 +0300

    add scripts *add slider function *add modal window function

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,9 @@ window.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger');
 
     // When it active it stop window scroll
-    hamburger.addEventListener('click', () => {
+    hamburger.addEventListener('click', showHideMenu);
+
+    function showHideMenu() {
         if (hamburger.classList.contains('hamburger_active')) {
             menu.classList.remove('menu__active');
             hamburger.classList.remove('hamburger_active');
@@ -14,7 +16,40 @@ window.addEventListener('DOMContentLoaded', () => {
             hamburger.classList.add('hamburger_active');
             document.body.style.overflow = 'hidden';
         }
+    }
+
+    // Slider
+    const slides = document.querySelectorAll('.carousel-item');
+    const indicators = document.querySelectorAll('[data-target="Indicators"]');
+    let currentSlide = 0;
+    // let slideInterval = setInterval(slide, 5000);
+
+    function slide() {
+        slides[currentSlide].classList.remove('active');
+        indicators[currentSlide].classList.remove('i_active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+        indicators[currentSlide].classList.add('i_active');
+    }
+
+
+    // Modal
+    const btn = document.querySelectorAll('.btn');
+    const modalClose = document.querySelector('#modal_close');
+    const modal = document.querySelector('.overlay');
+
+    btn.forEach((item) => {
+        item.addEventListener('click', showHideModal);
     });
 
+    function showHideModal() {
+        modal.classList.add('overlay_active');
+        document.body.style.overflow = 'hidden';
+
+        modalClose.addEventListener('click', () => {
+            modal.classList.remove('overlay_active');
+            document.body.style.overflow = '';
+        });
+    }
 
 });
